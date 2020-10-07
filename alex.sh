dba33b56262e9e8a595b4ad3596e227243df781a
alex-coffen <182434085@qq.com>
2020-10-07 18:05:02 +0800

    201007

--- alex.sh
+++ alex.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+cd /root/youTube_ads_4_pi-hole
 git fetch upstream
 git checkout master
-git merge upstream/master
+git merge upstream/master -m "merge $(date +%y%m%d)"
 cat youtubelist.txt | sed 's/^/||/' | sed 's/$/^/' > youtubelist_adhome.txt
 git add .
-git commit -m "$(date)"
+git commit -m "$(date +%y%m%d)"
 git pm
 
